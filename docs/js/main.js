5cf5a952aea668d7653100ae136d3462672925fb
Andrey <andreyka-kim-1997@mail.ru>
2020-01-31 18:20:27 +0900

    pagination

--- docs/js/main.js
+++ docs/js/main.js
@@ -76,3 +76,17 @@ function ajaxFormSubmit() {
     return false;
 }
 
+
+$(document).ready(function (){
+    $('#page-nav').onePageNav({
+        currentClass:'active',
+        changeHash:false,
+        scrollSpeed:500,
+        scrollThreshold:0.5,
+        filter: '',
+        easing: 'swing',
+        begin: function () {},
+        end: function () {},
+        scrollChange: function ($currentListItem) {}
+    });
+})
