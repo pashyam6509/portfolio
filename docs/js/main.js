a3052d6c909db14fa5d90bcbd33a167e145a6859
Andrey <andreyka-kim-1997@mail.ru>
2020-02-03 01:01:50 +0900

    burger

--- docs/js/main.js
+++ docs/js/main.js
@@ -90,3 +90,30 @@ $(document).ready(function (){
         scrollChange: function ($currentListItem) {}
     });
 })
+
+
+function burgerMenu(selector) {
+    let menu = $(selector);
+    let button = menu.find('.burger-menu__button');
+    let links = menu.find('.burger-menu__link');
+    let overlay = menu.find('.burger-menu__overlay');
+
+    button.on('click', (e) => {
+        e.preventDefault();
+        toggleMenu();
+    });
+
+    links.on('click', () => toggleMenu());
+    overlay.on('click', () => toggleMenu());
+
+    function toggleMenu(){
+        menu.toggleClass('burger-menu_active');
+
+        if (menu.hasClass('burger-menu_active')){
+            $('body').css('overflow', 'hidden');
+        } else {
+            $('body').css('overflow', 'visible');
+        }
+    }
+}
+burgerMenu('.burger-menu');
